23a9145540d7bda8ab36a42d8c919acd2d63bec0
Vasilii <bogdanovva84@mail.ru>
2022-09-13 03:26:11 +0300

    Семинар 2 Задача 3

---
+++ HomeWorkSem02_Z03.py
@@ -0,0 +1,16 @@
+'''
+Задание 3 Задайте список из n чисел последовательности (1+1/n)^n и выведите на 
+экран их сумму, округлённую до трёх знаков после точки.
+Пример:
+•	Для n = 6 -> 14.072
+'''
+n = int(input('Enter number: ')) 
+
+def sequence(n):
+
+    return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
+        
+print(sequence(n))
+print(round(sum(sequence(n))))
+
+
