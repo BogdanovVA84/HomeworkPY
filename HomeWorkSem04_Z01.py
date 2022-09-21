bcc44d807f364c93b92912c933e0404dcf14fac7
Vasilii <bogdanovva84@mail.ru>
2022-09-21 23:31:08 +0300

    Семинар 4 Задача 1

---
+++ HomeWorkSem04_Z01.py
@@ -0,0 +1,19 @@
+'''
+Задача 1 Вычислить число π c заданной точностью d
+*Пример:* 
+
+- при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+https://completerepair.ru/kak-vychislit-chislo-pi
+
+'''
+import math
+
+pi = math.pi
+print('Pi = ', pi)
+d = 0.001
+print(f'Accuracy = {d}')
+count = 0
+while d < 1:
+    count += 1
+    d = d*10
+print(round(pi, count))
